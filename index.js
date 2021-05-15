647a869c581413310bf93f1d42c6bd31b49ff4fd
Min <mtan121@aucklanduni.ac.nz>
2021-05-15 15:29:44 +0800

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+var multiply
+let num1 = 31;
+let num2 = 2;
 
+multiply =num1*num2;
+
+let random=Math.floor(Math.random()+1);
+
+
+var mod
+let num3 = 28;
+let num4 = 6;
+
+mod = num3%num4;
+
+var max
+max = Math.max(5, 10, 15, 20);
 // Write your code here
